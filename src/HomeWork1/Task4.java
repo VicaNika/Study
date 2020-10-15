bb07eb9b8689e9cc419b40425ccb4bfaf3edb571
VicaNika <vica_nika@inbox.ru>
2020-10-15 13:01:48 +0300

    changed

--- src/HomeWork1/Task4.java
+++ src/HomeWork1/Task4.java
@@ -3,17 +3,24 @@ package HomeWork1;
 public class Task4 {
     public static void main(String[] args) {
 
-     if (sleepIn(false, true)) System.out.println("Можем спать");
-         if (sleepIn(true, true)) System.out.println("Можем спать");
-             if (sleepIn(true, false)) System.out.println("Пора идти на работу");
+     if (sleepIn(false, true))
 
+         System.out.println("Можем спать");
 
+         if (sleepIn(true, true))
 
+             System.out.println("Можем спать");
+
+             if (sleepIn(true, false))
+
+                 System.out.println("Пора идти на работу");
     }
 
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         if (!weekday || vacation) {
+
             return true;
+
         } else if (weekday || !vacation){
 
             return true;
